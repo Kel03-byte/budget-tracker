b7ba373a47e532c727ed6272ab9e70221ad35655
Kellie Ryan <littlekel03@gmail.com>
2021-07-18 06:27:03 +0800

    Updated connection to Mongo Cloud

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require("dotenv").config()
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"
 
 const app = express();
 
@@ -15,7 +18,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect( "mongodb://localhost/budget", {
+mongoose.connect( MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
